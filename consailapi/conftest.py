e99816c4b1efb353c44df4ca18968b1911c7759b
Mateusz Ernst <erniq1998@gmail.com>
2022-10-08 16:52:43 +0200

    Initial

---
+++ consailapi/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from consailapi.users.models import User
+from consailapi.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
