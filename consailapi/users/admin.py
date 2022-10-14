12b921007ff82d72e719af9a1e517b1e446543b8
jadwiga <jadwi.wrobel@gmail.com>
2022-10-14 21:34:59 +0200

    CS-37 User model cleanup

--- consailapi/users/admin.py
+++ consailapi/users/admin.py
@@ -15,7 +15,7 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserAdminCreationForm
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("name", "email")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
         (
             _("Permissions"),
             {
@@ -23,12 +23,14 @@ class UserAdmin(auth_admin.UserAdmin):
                     "is_active",
                     "is_staff",
                     "is_superuser",
-                    "groups",
-                    "user_permissions",
                 ),
             },
         ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (
+            _("Important dates"),
+            {"fields": ("last_login", "date_joined", "created_at", "updated_at")},
+        ),
     )
-    list_display = ["username", "name", "is_superuser"]
-    search_fields = ["name"]
+    list_display = ["email", "first_name", "last_name", "is_superuser"]
+    search_fields = ["last_name", "email"]
+    readonly_fields = ["updated_at", "created_at"]
