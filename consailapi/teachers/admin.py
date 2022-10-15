2998cbe6935b50e078f59968beb34c10fe3d0adf
jadwiga <jadwi.wrobel@gmail.com>
2022-10-15 17:55:51 +0200

    CS-18 Created Teacher model

---
+++ consailapi/teachers/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from consailapi.teachers.models import Teacher
+from consailapi.users.admin import UserAdmin
+
+
+@admin.register(Teacher)
+class TeacherAdmin(UserAdmin):
+    list_display = ["email", "degrees", "first_name", "last_name", "uuid", "created_at"]
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("degrees", "first_name", "last_name", "name_display", "email")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+        (
+            _("Important dates"),
+            {"fields": ("last_login", "date_joined", "created_at", "updated_at")},
+        ),
+    )
+    search_fields = ["last_name", "email"]
+    readonly_fields = ["updated_at", "created_at"]
