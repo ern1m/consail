63bb12ebcaeaddeec14d4ab091a8f2f4e786a39f
jadwiga <jadwi.wrobel@gmail.com>
2022-10-17 23:34:25 +0200

    CS-33 Created base logout endpoint

--- consailapi/authentication/api/urls.py
+++ consailapi/authentication/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from consailapi.authentication.api.views import login
+from consailapi.authentication.api.views import login, logout
 
 app_name = "auth"
-urlpatterns = [path("login/", login, name="login")]
+urlpatterns = [
+    path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
+]
