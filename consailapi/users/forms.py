fd520729f3d59feaa5fec7aa68fbf7d21a3de290
Mateusz Ernst <erniq1998@gmail.com>
2022-10-19 09:06:53 +0200

    CS-29 Added Register endpoint

--- consailapi/users/forms.py
+++ consailapi/users/forms.py
@@ -1,7 +1,6 @@
-from allauth.account.forms import SignupForm
-from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django import forms
 from django.contrib.auth import forms as admin_forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, password_validation
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
@@ -18,25 +17,37 @@ class UserAdminCreationForm(admin_forms.UserCreationForm):
     To change user signup, see UserSignupForm and UserSocialSignupForm.
     """
 
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+    )
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        help_text=_("Enter the same password as before, for verification."),
+    )
+
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
+        fields = ("email", "password1", "password2")
 
-        error_messages = {
-            "username": {"unique": _("This username has already been taken.")}
-        }
-
-
-class UserSignupForm(SignupForm):
-    """
-    Form that will be rendered on a user sign up section/screen.
-    Default fields will be added automatically.
-    Check UserSocialSignupForm for accounts created from social.
-    """
-
-
-class UserSocialSignupForm(SocialSignupForm):
-    """
-    Renders the form when user has signed up using social accounts.
-    Default fields will be added automatically.
-    See UserSignupForm otherwise.
-    """
+    # def save(self, *args, **kwargs):
+    #     """
+    #     Save user. Create if necessary.
+    #     :param commit:
+    #     :return:
+    #     """
+    #
+    #     self.instance, _c = User.objects.update_or_create(
+    #         email=self.cleaned_data["email"],
+    #         defaults={
+    #             "first_name": self.cleaned_data["first_name"],
+    #             "last_name": self.cleaned_data["last_name"],
+    #             "username": self.cleaned_data["email"],
+    #             "is_active": False,
+    #         },
+    #     )
