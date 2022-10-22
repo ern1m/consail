c1536cb521b74663ca9c7d3e0a4e538cc1e1ad1d
Mateusz Ernst <erniq1998@gmail.com>
2022-10-23 00:22:01 +0200

    CS-35 Removed unnecessary django admin sites

--- consailapi/shared/admin.py
+++ consailapi/shared/admin.py
@@ -1,1 +1,20 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.sites.models import Site
+from django_celery_beat.models import (
+    ClockedSchedule,
+    CrontabSchedule,
+    IntervalSchedule,
+    PeriodicTask,
+    SolarSchedule,
+)
+from rest_framework.authtoken.models import TokenProxy
+
+admin.site.unregister(ClockedSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(SolarSchedule)
+admin.site.unregister(TokenProxy)
+admin.site.unregister(Group)
+admin.site.unregister(Site)
