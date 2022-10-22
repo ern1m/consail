e1e220fcbee7b0f59a26a14bc5cbaed4c19eaeec
Mateusz Ernst <erniq1998@gmail.com>
2022-10-23 00:24:29 +0200

    CS-29 Added Validators to register endpoint

--- consailapi/students/api/serializers.py
+++ consailapi/students/api/serializers.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework import serializers
 
 from consailapi.students.models import Student
@@ -8,17 +9,21 @@ class RegisterStudentSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
-    password = serializers.CharField(required=True)
+    password = serializers.CharField(
+        required=True, validators=[settings.AUTH_PASSWORD_VALIDATORS]
+    )
     repeat_password = serializers.CharField(required=True)
 
 
 class StudentSerializer(UserSerializer):
+    username = serializers.CharField(write_only=True)
     is_active = serializers.BooleanField(write_only=True)
 
     class Meta:
         model = Student
         fields = [
             *UserSerializer.Meta.fields,
+            "username",
             "password",
             "is_active",
         ]
