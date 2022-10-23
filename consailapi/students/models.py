75a5c5c1db1eda7e8921eae7d8d6e20a060c17f9
Mateusz Ernst <erniq1998@gmail.com>
2022-10-23 14:18:17 +0200

    CS-45 Added year field to model Student

--- consailapi/students/models.py
+++ consailapi/students/models.py
@@ -1,9 +1,12 @@
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from consailapi.users.models import User
 
 
 class Student(User):
+    year = models.PositiveSmallIntegerField(_("Year of study"), default=0)
+
     class Meta:
         verbose_name = _("student")
         verbose_name_plural = _("students")
