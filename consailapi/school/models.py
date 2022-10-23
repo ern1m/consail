274f282c3170d6936cd43839c8b516aaa4b9c514
Mateusz Ernst <erniq1998@gmail.com>
2022-10-23 15:03:25 +0200

    CS-45 Added MajorModel and DepartmentModel to school app

---
+++ consailapi/school/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from consailapi.students.models import Student
+
+
+class Department(models.Model):
+    name = models.CharField(_("Department name"), blank=True, max_length=100)
+
+    class Meta:
+        verbose_name = _("Department")
+        verbose_name_plural = _("Departments")
+
+    def __str__(self):
+        return self.name
+
+
+class Major(models.Model):
+    name = models.CharField(_("Major"), blank=True, max_length=100)
+    student = models.ForeignKey(
+        Student,
+        null=True,
+        verbose_name=_("Students Major"),
+        on_delete=models.SET_NULL,
+        related_name="major",
+    )
+    department = models.ForeignKey(
+        Department,
+        null=True,
+        verbose_name=_("Major Department"),
+        on_delete=models.SET_NULL,
+        related_name="major",
+    )
+
+    class Meta:
+        verbose_name = _("Major")
+        verbose_name_plural = _("Majors")
+
+    def __str__(self):
+        return self.name
