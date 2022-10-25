12dd6de5f6d0abd7a74419b84c9cd27a5c9eea09
Mateusz Ernst <erniq1998@gmail.com>
2022-10-25 19:10:55 +0200

    Swagger dev link changed

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -185,7 +185,7 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
     "SERVERS": [
-        {"url": "https://consail.site/", "description": "Local Development server"},
+        {"url": "https://api.consail.site/", "description": "Development server"},
     ],
 }
 
