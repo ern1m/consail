6d5269745fa6a5ee663f156446e5d0ed8a73280a
Mateusz Ernst <erniq1998@gmail.com>
2022-10-25 18:15:39 +0200

    CS-48 Added Validation to password

--- consailapi/students/api/serializers.py
+++ consailapi/students/api/serializers.py
@@ -1,4 +1,4 @@
-# from django.conf import settings
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from consailapi.school.api.serializers import MajorSerializer
@@ -10,10 +10,12 @@ class RegisterStudentSerializer(serializers.Serializer):
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
-    password = serializers.CharField(
-        required=True
-    )  # validators=[x.items for x in settings.AUTH_PASSWORD_VALIDATORS])
+    password = serializers.CharField(required=True)
     repeat_password = serializers.CharField(required=True)
+
+    def validate_password(self, password: str):
+        validate_password(password)
+        return password
 
 
 class StudentSerializer(UserSerializer):
