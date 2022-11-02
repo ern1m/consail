9936020fa117c2f6327332ae55ddbdf8136b23f2
jadwiga <jadwi.wrobel@gmail.com>
2022-11-02 17:36:26 +0100

    CS-73 Update lesson endpoint

--- consailapi/lessons/api/views.py
+++ consailapi/lessons/api/views.py
@@ -3,7 +3,12 @@ from typing import Any
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework.mixins import (
+    CreateModelMixin,
+    DestroyModelMixin,
+    ListModelMixin,
+    UpdateModelMixin,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -30,7 +35,9 @@ class SubjectViewSet(viewsets.GenericViewSet, ListModelMixin):
     ]
 
 
-class LessonViewSet(viewsets.GenericViewSet, CreateModelMixin, DestroyModelMixin):
+class LessonViewSet(
+    viewsets.GenericViewSet, CreateModelMixin, DestroyModelMixin, UpdateModelMixin
+):
     serializer_class = LessonActionSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     queryset = Lesson.objects.all().select_related("teacher", "subject")
@@ -43,6 +50,10 @@ class LessonViewSet(viewsets.GenericViewSet, CreateModelMixin, DestroyModelMixin
         "id",
     ]
 
+    def get_queryset(self):
+        teacher = self.request.user.teacher  # noqa
+        return self.queryset.filter(teacher=teacher)
+
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
         teacher = request.user.teacher  # noqa
         serializer = self.get_serializer(data=request.data)
@@ -53,7 +64,18 @@ class LessonViewSet(viewsets.GenericViewSet, CreateModelMixin, DestroyModelMixin
         )
 
     def destroy(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        teacher = request.user.teacher  # noqa
-        lesson = self.queryset.filter(uuid=kwargs.get("uuid"), teacher=teacher).first()
+        lesson = self.get_object()
         LessonService(lesson=lesson).delete()
         return Response(status=status.HTTP_200_OK)
+
+    def update(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        partial = kwargs.pop("partial", False)
+        lesson = self.get_object()
+        serializer = self.get_serializer(data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        lesson = LessonService(lesson).update(serializer.validated_data)
+        return Response(LessonBaseSerializer(lesson).data, status=status.HTTP_200_OK)
+
+    def partial_update(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        kwargs["partial"] = True
+        return self.update(request, *args, **kwargs)
