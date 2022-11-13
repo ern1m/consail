d393776b55f87c9ecb04698f371851443dbb4630
jadwi2000 <94324500+jadwi2000@users.noreply.github.com>
2022-11-13 19:47:08 +0100

    Feature/cs 64/reservations (#84)
    * CS-64 ReservationType model
    
    * CS-64 Reservation, ReservationType, ReservationSlot models
    
    * CS-64 Fixed migrations
    
    Co-authored-by: Mateusz Ernst <erniq1998@gmail.com>

--- consailapi/consultations/admin.py
+++ consailapi/consultations/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from consailapi.consultations.models import Consultation
+from consailapi.consultations.models import (
+    Consultation,
+    Reservation,
+    ReservationSlot,
+    ReservationType,
+)
 
 
 @admin.register(Consultation)
@@ -13,3 +18,29 @@ class ConsultationAdmin(admin.ModelAdmin):
     list_select_related = [
         "teacher",
     ]
+
+
+@admin.register(ReservationType)
+class ReservationTypeAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "uuid",
+    ]
+    list_select_related = [
+        "teacher",
+    ]
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "uuid",
+    ]
+    list_select_related = ["teacher", "student"]
+
+
+@admin.register(ReservationSlot)
+class ReservationSlot(admin.ModelAdmin):
+    readonly_fields = [
+        "uuid",
+    ]
+    list_select_related = ["consultation", "reservation"]
